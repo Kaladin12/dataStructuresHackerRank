1d1df8945cdbafda4660190ee2ad9a0b52e77d5d
Kaladin12 <eliancruz998@gmail.com>
2020-12-03 12:12:15 -0800

    duh

--- nose.py
+++ nose.py
@@ -1,6 +1,4 @@
 import math
-import re
-    
 def solve(n, k, roads):
     myGraph = {}
     nodeEdge1 = None
@@ -61,96 +59,6 @@ def solve(n, k, roads):
         else:
             values.append((dijkstra(nodeEdge1,i),0))
 
-    def biseccionChafa(INFER, SUPER):
-        if (n==2):
-            v =edgeNeeded/2
-            return [float('%0.05f' % v), float('%0.05f' % v)]
-        inf, sup = INFER, SUPER
-        while (sup-inf >0.00001 or sup==inf or sup==0):
-            currentInf = inf + (sup-inf)/4
-            currentSup = inf +3*(sup-inf)/4
-            resInf, resSup = [], []
-            for i in values:
-                currentMin = min(i[0], i[1])
-                u = edgeNeeded-currentInf + i[1]#resInf.append(edgeNeeded-currentInf + i[1])
-                v = edgeNeeded-currentSup + i[1]#resSup.append(edgeNeeded-currentSup + i[1])
-                u1 = i[0]+currentInf#resInf.append(i[0]+currentInf)
-                v1 = i[0]+currentSup#resSup.append(i[0]+currentSup)
-                resInf.append(min(u,u1))
-                resSup.append(min(v,v1))
-            deviationInf = max(resInf)
-            deviationSup = max(resSup)
-            nextV = (inf+sup)/2
-            if (deviationInf<=deviationSup):
-                sup = nextV
-            else:
-                inf = nextV
-        return [float('%0.05f' % round(min(inf,sup))),float('%0.05f' % round(max(resInf)) if min(inf,sup)==inf else round(max(resSup)))]
-    dis = biseccionChafa(0, edgeNeeded)
-    return("%0.05f" %(dis[0]),"%0.05f" % (dis[1]))
-
-#2
-#2 1 1
-#1 2 10
-#4 4 1
-#1 2 10
-#2 3 10
-#3 4 1
-#4 1 5
-#dasRoads = [
-#    [1, 2, 10],
-#    [2, 3, 10],
-#    [3, 4, 1],
-#    [4, 1, 5]
-#]
-#dasRoads = [[1,2,10]]
-#solve(2,1,dasRoads)
-#dasRoads = [
-#    [1, 2, 10],
-#    [2, 3, 10],
-#    [3, 4, 1],
-#    [4, 1, 5]
-#]
-#solve(4,1,dasRoads)
-#dasRoads = [
-#    [3, 7, 43],
-#[7, 10, 91],
-#[10, 1, 17],
-#[1, 9, 86],
-#[9, 5, 98],
-#[5, 4, 85],
-#[4, 8, 48],
-#[8, 6, 94],
-#[6, 2, 57],
-#[5, 2, 80]
-#]
-#solve(10,4,dasRoads)
-#dasRoads = [
-#    [6, 9, 93],
-#[9, 4, 18],
-#[4 ,3 ,80],
-#[3, 7, 94],
-#[7, 1, 68],
-#[1, 5, 58],
-#[5 ,10, 72],
-#[10, 8, 45],
-#[8, 2 ,93],
-#[7, 10, 72],
-#]
-#solve(10,8,dasRoads)
-#dasRoads = [
-#    [1, 7, 77],
-#[7, 8, 1],
-#[8 ,10, 85],
-#[10, 2, 21],
-#[2, 4, 51],
-#[4, 5, 66],
-#[5, 9, 86],
-#[9, 6, 89],
-#[6, 3, 26],
-#[9, 3, 70]
-#]
-#solve(10,5,dasRoads)
 f = open("txt\\test_2_4.txt", "r")
 v = []
 environVars = []
@@ -171,4 +79,4 @@ for x in f:
                 environVars.append(int(word))
     count +=1
 print(v[environVars[2]-1])
-print(solve(environVars[0],environVars[2],v))
+solve(environVars[0],environVars[2],v)
